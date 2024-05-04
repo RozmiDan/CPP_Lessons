5b74612df521cb8f9f27a925415ba5c05436a371
RozmiDan <rozmirski@mail.ru>
2024-05-04 22:00:36 +0300

    shared v2

--- my_shared_pointer/main.cpp
+++ my_shared_pointer/main.cpp
@@ -60,21 +60,19 @@ public:
         std::swap(counter, other.counter);
     }
 
+private:
+
+    template <typename ...Args>
+    my_shared_ptr<T> make_my_shared_ptr(Args&&... args); // делаем другом, чтобы уметь создавать
+                                                         // sh_ptr из ф-ии make_shared()
     struct ControlBlock
     {
-        ControlBlock(T data, size_t counter) : data(data), counter(counter){}
         T data;
         size_t counter;
     };
 
-private:
-
-    my_shared_ptr(ControlBlock){}
-
-    template <typename ...Args>
-    my_shared_ptr<T> make_my_shared_ptr(Args&&... args);
-
-
+    my_shared_ptr(ControlBlock* ptr):cbl(ptr){} // заприваченный конструктор, который может
+                                                // вызвать только make_shared()
     ControlBlock* cbl;
     T* data;
     size_t* counter;
@@ -83,31 +81,24 @@ private:
 template <typename T, typename ...Args>
 my_shared_ptr<T> make_my_shared_ptr(Args&&... args)
 {
-    //return  my_shared_ptr<T>(my_shared_ptr<T>(new T (args), new size_t (1)));
+    auto* cntrl_blck = new typename my_shared_ptr<T>::ControlBlock(T(std::forward<Args>(args)...), 1); // выделяем память под ControlBlock
+    return my_shared_ptr<T>(cntrl_blck);                                                              // создаемся от приватного констр.
 }
-
-template <typename T>
-struct ControlBloc
-{
-    T data;
-    size_t counter;
-};
-
 
 int main()
 {
-    auto p = ControlBloc<int>{int(23),1};
-    std::cout << p.data;
+//    auto p = ControlBloc<int>{int(23),1};
+//    std::cout << p.data;
 
-//    auto p = std::shared_ptr<int>(new int (234));
-//    auto q(p);
-//    auto o = q;                 // p = q = o -> 234
-//    *o = 23;                    // p = q = o -> 23
-//    auto j(std::move(o));       // p = q = j -> 23, o -> nullptr
+    auto p = std::shared_ptr<int>(new int (234));
+    auto q(p);
+    auto o = q;                 // p = q = o -> 234
+    *o = 23;                    // p = q = o -> 23
+    auto j(std::move(o));       // p = q = j -> 23, o -> nullptr
 
-//    std::cout << *p <<' '<< *q << ' ' << *j << ' ' << *o;
+    std::cout << *p <<' '<< *q << ' ' << *j << ' ' << *o;
 
-//    auto a = std::make_shared<int>(4);
+    auto a = std::make_shared<int>(4);
 
 
     return 0;
